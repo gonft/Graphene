3cae4185a119123f2094b42a68a3dcde36255ead
proghjy <proghjy@gmail.com>
2018-10-04 18:44:36 +0900

    #2 Testing our GraphQL schema

--- cookbook/schema.py
+++ cookbook/schema.py
@@ -1,11 +1,8 @@
 import graphene
-
 import cookbook.ingredients.schema
 
 
 class Query(cookbook.ingredients.schema.Query, graphene.ObjectType):
-    # This class will inherit from multiple Queries
-    # as we begin to add more apps to our project
     pass
 
 schema = graphene.Schema(query=Query)
