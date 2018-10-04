4066dad2990f1a22605c8d29f6d77aaa8279b771
proghjy <proghjy@gmail.com>
2018-10-04 16:23:26 +0900

    #2 Defining our models

--- cookbook/ingredients/models.py
+++ cookbook/ingredients/models.py
@@ -14,7 +14,7 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField()
     category = models.ForeignKey(
-        Category, related_name='ingredients', on_delete=models.CASCADE
+        Category, related_name='ingredients'
     )
 
     def __str__(self):
