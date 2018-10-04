122fa711309c97cf07878aded2885e296531aaa6
proghjy <proghjy@gmail.com>
2018-10-04 18:43:34 +0900

    #2 Create cookbook/ingredients/schema.py

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -1,28 +1,36 @@
 # cookbook/ingredients/schema.py
-import graphene
-
-from graphene_django.types import DjangoObjectType
+from graphene import relay, ObjectType
+from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from cookbook.ingredients.models import Category, Ingredient
 
 
-class CategoryType(DjangoObjectType):
+# Graphene will automatically map the Category model's fields onto the CategoryNode.
+# This is configured in the CategoryNode's Meta class (as you can see below)
+class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
+        filter_fields = ['name', 'ingredients']
+        interfaces = (relay.Node, )
 
 
-class IngredientType(DjangoObjectType):
+class IngredientNode(DjangoObjectType):
     class Meta:
         model = Ingredient
+        # Allow for some more advanced filtering here
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'notes': ['exact', 'icontains'],
+            'category': ['exact'],
+            'category__name': ['exact'],
+        }
+        interfaces = (relay.Node, )
 
 
 class Query(object):
-    all_categories = graphene.List(CategoryType)
-    all_ingredients = graphene.List(IngredientType)
+    category = relay.Node.Field(CategoryNode)
+    all_categories = DjangoFilterConnectionField(CategoryNode)
 
-    def resolve_all_categories(self, info, **kwargs):
-        return Category.objects.all()
-
-    def resolve_all_ingredients(self, info, **kwargs):
-        # We can easily optimize query count in the resolve method
-        return Ingredient.objects.select_related('category').all()
+    ingredient = relay.Node.Field(IngredientNode)
+    all_ingredients = DjangoFilterConnectionField(IngredientNode)
